4c2fc7598db9f17ed6e317c241936154145330d4
Mikolaj Fido <mikolaj.fido@gmail.com>
2025-04-02 10:17:17 +0100

    re-organize code into separate modules

--- app/internal/symbols_helpers.py
+++ app/internal/symbols_helpers.py
@@ -1,62 +1,13 @@
+from _operator import attrgetter
 from itertools import groupby
-from operator import attrgetter
-from typing import TypeAlias
 
-from pydantic import NaiveDatetime, model_validator, BaseModel
-from sqlalchemy import DateTime
-from sqlmodel import SQLModel, Field
-
-from app.constants import LOWEST_DATETIME, HIGHEST_DATETIME
-from app.internal.id_generator import generate_ref_data_uuid
-
-
-class SymbologySymbolSpec(SQLModel):
-    symbol: str
-    exchange: str | None = None
-    start_time: NaiveDatetime | None = Field(
-        default=LOWEST_DATETIME,
-        description=f"Start time of the symbol. Should be greater than {LOWEST_DATETIME}.",
-        ge=LOWEST_DATETIME,
-        sa_type=DateTime,
-        primary_key=True,
-    )
-    end_time: NaiveDatetime | None = Field(
-        default=HIGHEST_DATETIME,
-        description=f"End time of the symbol. Should be greater than {HIGHEST_DATETIME}.",
-        le=HIGHEST_DATETIME,
-        sa_type=DateTime,
-    )
-
-    @model_validator(mode="after")
-    def check_start_time_less_than_end_time(self):
-        if self.start_time >= self.end_time:
-            raise ValueError("Start time should be less than end time.")
-        return self
-
-
-class SymbologySymbolDb(SymbologySymbolSpec, table=True):
-    ref_data_uuid: str | None = Field(
-        default_factory=generate_ref_data_uuid, primary_key=True
-    )
-    symbology: str = Field(primary_key=True)
-
-
-SymbologyMaps: TypeAlias = dict[str, list[SymbologySymbolSpec]]
-
-
-class SymbologySymbolCreate(SQLModel):
-    """Create representation of the Symbology Symbol."""
-
-    symbology_map: SymbologyMaps
-    force_duplicates: bool = False
-
-
-class SymbologySymbolPublic(SymbologySymbolCreate):
-    """Public representation of the Symbology Symbol."""
-
-    ref_data_uuid: str
-    message: str | None = None
-    error: str | None = None
+from app.schemas import (
+    SymbologySymbolDb,
+    SymbologySymbolPublic,
+    SymbologySymbolSpec,
+    SymbologyMaps,
+    SymbolsToQuery,
+)
 
 
 def convert_list_of_db_objects_to_public_objects(
@@ -95,15 +46,6 @@ def convert_list_of_db_objects_to_public_objects(
         grouped_by_uuid.append(SymbologySymbolPublic(**temp_group))
 
     return grouped_by_uuid
-
-
-class SymbolsToQuery(BaseModel):
-    """This is used to find the symbols in the database, given user inputs."""
-
-    symbology: str
-    symbol: str
-    start_time: NaiveDatetime
-    end_time: NaiveDatetime
 
 
 def convert_symbology_maps_to_symbology_symbol_date_tuples(
